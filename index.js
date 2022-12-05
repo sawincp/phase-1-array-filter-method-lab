7bf3efa6dba19b101ab8d621f9be2da64b12ac81
Corey Sawin <sawincp@gmail.com>
2022-12-05 14:33:44 -0500

    commit tree 6cf9263ba3f243f12a5c82c921fb5e8e3e17cb27

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching (driverSource, driverExpected){
+    return driverSource.filter(
+        (possibleMatch)=> possibleMatch.toLowerCase() === driverExpected.toLowerCase()
+    )
+}
+
+function fuzzyMatch(driverSource, driverExpected){
+    return driverSource.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(driverExpected.toLowerCase())=== 0
+    )
+}
+
+function matchName(driverSource, driverExpected){
+    return driverSource.filter((record) => record.name === driverExpected 
+    )
+}
